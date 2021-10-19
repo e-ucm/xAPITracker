4402f5afabb986412ec6e8541e464e1583ab69bf
Victorma <victorma-fire@hotmail.com>
2021-10-19 02:57:46 +0200

    Added async LRS interface

---
+++ xAPIClient/AsyncLRS.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TinCan.Documents;
+using TinCan.LRSResponses;
+
+namespace TinCan
+{
+    public class AsyncLRS : IAsyncLRS
+    {
+        public ILRS lrs;
+
+        public Task<AboutLRSResponse> About()
+        {
+            return Task.FromResult(lrs.About());
+        }
+
+        public Task<LRSResponse> ClearState(Activity activity, Agent agent, Guid? registration = null)
+        {
+            return Task.FromResult(lrs.ClearState(activity, agent, registration));
+        }
+
+        public Task<LRSResponse> DeleteActivityProfile(ActivityProfileDocument profile)
+        {
+            return Task.FromResult(lrs.DeleteActivityProfile(profile));
+        }
+
+        public Task<LRSResponse> DeleteAgentProfile(AgentProfileDocument profile)
+        {
+            return Task.FromResult(lrs.DeleteAgentProfile(profile));
+        }
+
+        public Task<LRSResponse> DeleteState(StateDocument state)
+        {
+            return Task.FromResult(lrs.DeleteState(state));
+        }
+
+        public Task<StatementsResultLRSResponse> MoreStatements(StatementsResult result)
+        {
+            return Task.FromResult(lrs.MoreStatements(result));
+        }
+
+        public Task<StatementsResultLRSResponse> QueryStatements(StatementsQuery query)
+        {
+            return Task.FromResult(lrs.QueryStatements(query));
+        }
+
+        public Task<ActivityProfileLRSResponse> RetrieveActivityProfile(string id, Activity activity)
+        {
+            return Task.FromResult(lrs.RetrieveActivityProfile(id, activity));
+        }
+
+        public Task<ProfileKeysLRSResponse> RetrieveActivityProfileIds(Activity activity)
+        {
+            return Task.FromResult(lrs.RetrieveActivityProfileIds(activity));
+        }
+
+        public Task<AgentProfileLRSResponse> RetrieveAgentProfile(string id, Agent agent)
+        {
+            return Task.FromResult(lrs.RetrieveAgentProfile(id, agent));
+        }
+
+        public Task<ProfileKeysLRSResponse> RetrieveAgentProfileIds(Agent agent)
+        {
+            return Task.FromResult(lrs.RetrieveAgentProfileIds(agent));
+        }
+
+        public Task<StateLRSResponse> RetrieveState(string id, Activity activity, Agent agent, Guid? registration = null)
+        {
+            return Task.FromResult(lrs.RetrieveState(id, activity, agent, registration));
+        }
+
+        public Task<ProfileKeysLRSResponse> RetrieveStateIds(Activity activity, Agent agent, Guid? registration = null)
+        {
+            return Task.FromResult(lrs.RetrieveStateIds(activity, agent, registration));
+        }
+
+        public Task<StatementLRSResponse> RetrieveStatement(Guid id)
+        {
+            return Task.FromResult(lrs.RetrieveStatement(id));
+        }
+
+        public Task<StatementLRSResponse> RetrieveVoidedStatement(Guid id)
+        {
+            return Task.FromResult(lrs.RetrieveVoidedStatement(id));
+        }
+
+        public Task<LRSResponse> SaveActivityProfile(ActivityProfileDocument profile)
+        {
+            return Task.FromResult(lrs.SaveActivityProfile(profile));
+        }
+
+        public Task<LRSResponse> SaveAgentProfile(AgentProfileDocument profile)
+        {
+            return Task.FromResult(lrs.SaveAgentProfile(profile));
+        }
+
+        public Task<LRSResponse> SaveState(StateDocument state)
+        {
+            return Task.FromResult(lrs.SaveState(state));
+        }
+
+        public Task<StatementLRSResponse> SaveStatement(Statement statement)
+        {
+            return Task.FromResult(lrs.SaveStatement(statement));
+        }
+
+        public Task<StatementsResultLRSResponse> SaveStatements(List<Statement> statements)
+        {
+            return Task.FromResult(lrs.SaveStatements(statements));
+        }
+
+        public Task<StatementLRSResponse> VoidStatement(Guid id, Agent agent)
+        {
+            return Task.FromResult(lrs.VoidStatement(id, agent));
+        }
+    }
+}
---
+++ xAPIClient/IAsyncLRS.cs
@@ -0,0 +1,52 @@
+﻿/*
+    Copyright 2014 Rustici Software
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TinCan.Documents;
+using TinCan.LRSResponses;
+
+namespace TinCan
+{
+    public interface IAsyncLRS
+    {
+        Task<AboutLRSResponse> About();
+
+        Task<StatementLRSResponse> SaveStatement(Statement statement);
+        Task<StatementLRSResponse> VoidStatement(Guid id, Agent agent);
+        Task<StatementsResultLRSResponse> SaveStatements(List<Statement> statements);
+        Task<StatementLRSResponse> RetrieveStatement(Guid id);
+        Task<StatementLRSResponse> RetrieveVoidedStatement(Guid id);
+        Task<StatementsResultLRSResponse> QueryStatements(StatementsQuery query);
+        Task<StatementsResultLRSResponse> MoreStatements(StatementsResult result);
+
+        Task<ProfileKeysLRSResponse> RetrieveStateIds(Activity activity, Agent agent, Nullable<Guid> registration = null);
+        Task<StateLRSResponse> RetrieveState(String id, Activity activity, Agent agent, Nullable<Guid> registration = null);
+        Task<LRSResponse> SaveState(StateDocument state);
+        Task<LRSResponse> DeleteState(StateDocument state);
+        Task<LRSResponse> ClearState(Activity activity, Agent agent, Nullable<Guid> registration = null);
+
+        Task<ProfileKeysLRSResponse> RetrieveActivityProfileIds(Activity activity);
+        Task<ActivityProfileLRSResponse> RetrieveActivityProfile(String id, Activity activity);
+        Task<LRSResponse> SaveActivityProfile(ActivityProfileDocument profile);
+        Task<LRSResponse> DeleteActivityProfile(ActivityProfileDocument profile);
+
+        Task<ProfileKeysLRSResponse> RetrieveAgentProfileIds(Agent agent);
+        Task<AgentProfileLRSResponse> RetrieveAgentProfile(String id, Agent agent);
+        Task<LRSResponse> SaveAgentProfile(AgentProfileDocument profile);
+        Task<LRSResponse> DeleteAgentProfile(AgentProfileDocument profile);
+    }
+}
